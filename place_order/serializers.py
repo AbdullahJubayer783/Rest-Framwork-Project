d36b44d127b13de579994b9bea5db094757bef7a
Abdullah jubayer <abdullahjubayer783@gmail.com>
2024-01-30 13:46:14 +0600

    remove

--- place_order/serializers.py
+++ place_order/serializers.py
@@ -2,8 +2,8 @@ from rest_framework import serializers
 from . import models
 
 class OrderDetailsSerializer(serializers.ModelSerializer):
-    flower = serializers.StringRelatedField(many=False)
-    user = serializers.StringRelatedField(many=False)
+    # flower = serializers.StringRelatedField(many=False)
+    # user = serializers.StringRelatedField(many=False)
     class Meta:
         model = models.OrderDetails
         fields = '__all__'
